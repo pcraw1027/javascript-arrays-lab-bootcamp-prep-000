351463b5bb19d99ec2d8df2fb1edea3cd240d3ea
Peter Crawford <pcraw1027@gmail.com>
2021-01-30 21:49:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten() {
+  var newkittens = [...kittens,"Broom"]
+}
+
+function prependKitten() {
+  var newkittens = ["Arnold",...kittens]
+}
